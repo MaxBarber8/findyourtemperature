88a3a994bb3a10716351d3aea07be8aa3d049257
Max Barber <maxwellbarber1@gmail.com>
2023-04-14 20:48:03 -0400

    Completed homework steps for week 11

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('hello from backend to frontend!');
+})
+
+app.post('/weather', (req, res) => {
+    const {cityName} = req.body;
+    res.send('City name successfully received: ' + cityName)
 })
 
 app.listen(port, () => {
